9f294f97417d997a7bb852a54b6f9055d753ce9f
Tedko <teodorstanishev@gmail.com>
2018-12-14 18:02:07 +0200

    first_commit

--- lib/BLE/BLE.cpp
+++ lib/BLE/BLE.cpp
@@ -14,93 +14,34 @@ void BLE::begin()
     BLEDevice::init(deviceName);
     //Change power level for RELEASE
     BLEDevice::setPower(ESP_PWR_LVL_P7);
+    // BLEDevice::setMTU(500);
     //
     pServer = BLEDevice::createServer();
     pServer->setCallbacks(new ServerCallbacks(this));
     pCallback = new CharacteristicCallback(this);
 
-    manufacturerService = pServer->createService(MANUFACTURER_SERVICE_UUID);
-    manufacturerName = manufacturerService->createCharacteristic(MANUFACTURER_NAME_UUID, BLECharacteristic::PROPERTY_READ);
 
-    //Create the service
+    // manufacturerService = pServer->createService(MANUFACTURER_SERVICE_UUID);
+    // manufacturerName = manufacturerService->createCharacteristic(MANUFACTURER_NAME_UUID, BLECharacteristic::PROPERTY_READ);
+    //Create the Car service
     carService = pServer->createService(CAR_SERVICE_UUID);
-    //Create the chararteristics for that service
-    //WINDOW LEFT
-    windows[WINDOW_LEFT] = carService->createCharacteristic(WINDOW_LEFT_CHARACTERISTIC_UUID,
+    //TEST max char value
+    pCharacteristic = carService->createCharacteristic(CHARACRERISTIC_UUID,
     BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE | BLECharacteristic::PROPERTY_NOTIFY);
-    windows[WINDOW_LEFT]->addDescriptor(new BLE2902());
-    windows[WINDOW_LEFT]->setCallbacks(pCallback);
-    //WINDOW RIGHT
-    windows[WINDOW_RIGHT] = carService->createCharacteristic(WINDOW_RIGHT_CHARACTERISTIC_UUID,
-    BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE | BLECharacteristic::PROPERTY_NOTIFY);
-    windows[WINDOW_RIGHT]->addDescriptor(new BLE2902());
-    windows[WINDOW_RIGHT]->setCallbacks(pCallback);
-    //IGNITION
-    ignition = carService->createCharacteristic(IGNITION_CHARACTERISTIC_UUID,
-    BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE | BLECharacteristic::PROPERTY_NOTIFY);
-    ignition->addDescriptor(new BLE2902());
-    ignition->setCallbacks(pCallback);
-    //LOCK CONTROL
-    lockControl = carService->createCharacteristic(LOCK_CONTROL_CHARACTERISTIC_UUID,
-    BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE | BLECharacteristic::PROPERTY_NOTIFY);
-    lockControl->addDescriptor(new BLE2902());
-    lockControl->setCallbacks(pCallback);
-    //
-    //CLOCK
-    rtc_clock = carService->createCharacteristic(CLOCK_CHARACTERISTIC_UUID,
-    BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE);
-    rtc_clock->addDescriptor(new BLE2902());
-    rtc_clock->setCallbacks(pCallback);
-    //
-    //PIN CODE
-    pin.characteristic = carService->createCharacteristic(PIN_CODE_CHARACRERISTIC_UUID,
-    BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE);
-    pin.characteristic->addDescriptor(new BLE2902());
-    pin.characteristic->setCallbacks(pCallback);
-    //
+    pCharacteristic->addDescriptor(new BLE2902());
+    pCharacteristic->setCallbacks(pCallback);
 
-    BLE::logCharacteristic = carService->createCharacteristic(LOG_CHARACTERISTIC_UUID,
-    BLECharacteristic::PROPERTY_READ);
-    logCharacteristic->addDescriptor(new BLE2902());
-    logCharacteristic->setCallbacks(pCallback);
 
-    //Set the default value
-    manufacturerName->setValue(MANUFACTURER_NAME);
-    manufacturerService->start();
-
-    //set default value for the windows and start the service
-    BLE::setDefaultAll();
+    pCharacteristic->setValue("1532");
 
     carService->start();
-    //
+    // //
 
     //Start advertising so you can search for it
     pAdvertising = pServer->getAdvertising();
     pAdvertising->start();
 }
 
-void BLE::writeLog(string s)
-{
-    _log += s + "\n";
-    Serial.print(_log.data());
-
-    //TODO
-    logCharacteristic->setValue(_log);
-}
-void BLE::readLog()
-{
-    Serial.println("READ LOG");
-}
-
-//DEBUG
-vector<string> BLE::getValues()
-{
-    vector<string> buff;
-    buff.push_back(windows[WINDOW_LEFT]->getValue());
-    buff.push_back(windows[WINDOW_RIGHT]->getValue());
-
-    return buff;
-}
 string BLE::getPinCode()
 {
     return pin.characteristic->getValue();
@@ -112,26 +53,16 @@ void BLE::clearPinCode()
 
 void BLE::setDefaultAll()
 {
-    windows[WINDOW_LEFT]->setValue(&STANDARD, sizeof(&STANDARD));
-    windows[WINDOW_RIGHT]->setValue(&STANDARD, sizeof(&STANDARD));
-    ignition->setValue(&STANDARD, sizeof(&STANDARD));
-    lockControl->setValue(&LOCK , sizeof(&LOCK));
+    
 }
 void BLE::notifyAll()
 {
     if (isConnected)
     {
-        windows[WINDOW_LEFT]->notify();
-        windows[WINDOW_RIGHT]->notify();
-        ignition->notify();
-        lockControl->notify();
+        pCharacteristic->notify();
     }
 }
 
-void BLE::blockedTimeout(void *p)
-{
-    vTaskDelay(10000);
-}
 
 void BLE::block()
 {
@@ -139,34 +70,57 @@ void BLE::block()
     // xTaskCreate(blockedTimeout, "Blocked timeout", 1024, (void *)1, tskIDLE_PRIORITY, NULL);
 }
 
+
+
+
+
 void CharacteristicCallback::onRead(BLECharacteristic *pCharacteristic)
 {
 }
 void CharacteristicCallback::onWrite(BLECharacteristic *pCharacteristic)
 {
-    string buffUUID = pCharacteristic->getUUID().toString();
-    // if (buffUUID == PIN_CODE_CHARACRERISTIC_UUID)
-    // {
-        if (c->getPinCode() != c->pin.PIN_CODE)
-        {
-            c->setDefaultAll();
-            c->pin.failedEntries++;
-            Serial.print("Failed entries: ");
-            Serial.println(c->pin.failedEntries);
-            c->block();
+    // string buffUUID = pCharacteristic->getUUID().toString();
+    // // if (buffUUID == PIN_CODE_CHARACRERISTIC_UUID)
+    // // {
+    //     if (c->getPinCode() != c->pin.PIN_CODE)
+    //     {
+    //         c->setDefaultAll();
+    //         c->pin.failedEntries++;
+    //         Serial.print("Failed entries: ");
+    //         Serial.println(c->pin.failedEntries);
+    //         c->block();
 
-        }
-        else if (c->getPinCode() == c->pin.PIN_CODE)
-        {
-            Serial.println("Password Correct!");
-            //Clear the failed attempts
-            c->pin.failedEntries = 0;
-            //
-        }
-    // }
-    if (c->pin.failedEntries >= c->pin.MAX_FAILED_ENTRIES)
-        c->block();
+    //     }
+    //     else if (c->getPinCode() == c->pin.PIN_CODE)
+    //     {
+    //         Serial.println("Password Correct!");
+    //         //Clear the failed attempts
+    //         c->pin.failedEntries = 0;
+    //         //
+    //     }
+    // // }
+    // if (c->pin.failedEntries >= c->pin.MAX_FAILED_ENTRIES)
+    //     c->block();
 
-    Serial.print(string_to_hex(pCharacteristic->getValue()).data());
+    Serial.print((pCharacteristic->getValue()).data());
     Serial.println();
+}
+
+void ClockCallback::onRead(BLECharacteristic *pCharacteristic)
+{
+    
+}
+void ClockCallback::onWrite(BLECharacteristic *pCharacteristic){
+    string bleTime = pCharacteristic->getValue();
+    if(bleTime!="0"){
+        struct tm tm;
+        tm.tm_year = atoi(bleTime.substr(0,3).c_str()) - 1900;
+        tm.tm_mon = atoi(bleTime.substr(5,6).c_str()) - 1;
+        tm.tm_mday = atoi(bleTime.substr(8,9).c_str());
+        tm.tm_hour = atoi(bleTime.substr(11,12).c_str());
+        tm.tm_min = atoi(bleTime.substr(14,15).c_str());
+        t = mktime(&tm);
+        struct timeval now = { .tv_sec = t };
+        settimeofday(&now , NULL);
+    }
 }
--- lib/BLE/BLE.hpp
+++ lib/BLE/BLE.hpp
@@ -6,6 +6,8 @@
 #include <BLEUtils.h>
 #include <BLEServer.h>
 #include <BLE2902.h>
+#include <sys/time.h>
+
 
 #include <FreeRTOS.h>
 #include <string>
@@ -15,27 +17,31 @@
 
 using namespace std;
 
-//Manufacturer service and characteristics
-#define MANUFACTURER_SERVICE_UUID "0000180a-0000-1000-8000-00805F9B34FB"
-#define MANUFACTURER_NAME_UUID "00002a29-0000-1000-8000-00805F9B34FB"
-#define MANUFACTURER_NAME "Teodor Stanishev"
-//
+// //Manufacturer service and characteristics
+// #define MANUFACTURER_SERVICE_UUID "0000180a-0000-1000-8000-00805F9B34FB"
+// #define MANUFACTURER_NAME_UUID "00002a29-0000-1000-8000-00805F9B34FB"
+// #define MANUFACTURER_NAME "Teodor Stanishev"
+// //
 
-//Car service and characteristics
+// //Car service and characteristics
 #define CAR_SERVICE_UUID "ca227c6b-d187-4aaf-b330-37144d84b02c"
-#define WINDOW_LEFT_CHARACTERISTIC_UUID "3ff8860e-72ca-4a25-9c4e-99c7d3b08e9b"
-#define WINDOW_RIGHT_CHARACTERISTIC_UUID "96cc6576-b9b0-443b-b8e6-546bbd20d374"
-#define IGNITION_CHARACTERISTIC_UUID "4ad1bbf1-b3e6-4239-a3bb-520624ee1329"
-#define LOCK_CONTROL_CHARACTERISTIC_UUID "aface04c-7963-43ec-a172-bedeb1b49570"
-#define CLOCK_CHARACTERISTIC_UUID "6bbdd85f-c398-4075-abad-62d3ba40916a"
+// #define WINDOW_LEFT_CHARACTERISTIC_UUID "3ff8860e-72ca-4a25-9c4e-99c7d3b08e9b"
+// #define WINDOW_RIGHT_CHARACTERISTIC_UUID "96cc6576-b9b0-443b-b8e6-546bbd20d374"
+// #define IGNITION_CHARACTERISTIC_UUID "4ad1bbf1-b3e6-4239-a3bb-520624ee1329"
+// #define LOCK_CONTROL_CHARACTERISTIC_UUID "aface04c-7963-43ec-a172-bedeb1b49570"
+// #define CLOCK_CHARACTERISTIC_UUID "6bbdd85f-c398-4075-abad-62d3ba40916a"
 
-#define LOG_CHARACTERISTIC_UUID "9c64b5fd-9d4c-49fe-99b0-b9cf4f091026"
-#define PIN_CODE_CHARACRERISTIC_UUID "def231dc-07d4-4a71-b735-811e07d44c07" 
+// #define PIN_CODE_CHARACRERISTIC_UUID "def231dc-07d4-4a71-b735-811e07d44c07" 
+#define CHARACRERISTIC_UUID "3ff8860e-72ca-4a25-9c4e-99c7d3b08e9b" 
+
 //
 
 //Constants
 #define WINDOW_LEFT 0
 #define WINDOW_RIGHT 1
+#define IGNITION 2
+#define LOCK 3
+#define 
 
 // TODO make it a variable
 // #define PIN_CODE "pticata"
@@ -45,25 +51,17 @@ using namespace std;
 class CharacteristicCallback;
 class BLE;
 class ServerCallbacks;
+class ClockCallback;
 
 
 
 class BLE{
     private:
     BLEServer *pServer;
-    BLEService *manufacturerService;
-    BLECharacteristic *manufacturerName;
     BLEService *carService;
-    BLECharacteristic *windows[2];
-    BLECharacteristic *ignition;
-    BLECharacteristic *lockControl;
-    BLECharacteristic *rtc_clock;
-
-    
-    //CharacteristicCallback characteristic (part of the car service)
-    BLECharacteristic *logCharacteristic;
+    BLECharacteristic *pCharacteristic;
     CharacteristicCallback *pCallback;
-    //
+    
 
     BLEAdvertising *pAdvertising;
 
@@ -113,6 +111,7 @@ class BLE{
     vector<string> getValues();
     string getPinCode();
     void clearPinCode();
+    string getGetState()
 
     void setDefaultAll();
     void notifyAll();
@@ -123,9 +122,21 @@ class BLE{
 };
 
 
+class ClockCallback : public BLECharacteristicCallbacks{
+private:
+std::time_t t = std::time(nullptr);
+BLE *c;
+public:
+    ClockCallback(BLE *c){
+        this->c = c;
+    }
+    void onRead(BLECharacteristic *pCharacteristic);
+    void onWrite(BLECharacteristic *pCharacteristic);
+};
+
+
 class CharacteristicCallback : public BLECharacteristicCallbacks{
     private:
-    std::time_t t = std::time(nullptr);
     //BLE car reference
     BLE *c;
 
